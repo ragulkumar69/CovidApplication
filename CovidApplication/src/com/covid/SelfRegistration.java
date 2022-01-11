f3b39b5a115a3343eba88d61b0d24a23560a35be
ragulkumar69 <92845042+ragulkumar69@users.noreply.github.com>
2022-01-11 12:54:26 +0530

    Update SelfRegistration.java

--- CovidApplication/src/com/covid/SelfRegistration.java
+++ CovidApplication/src/com/covid/SelfRegistration.java
@@ -5,18 +5,18 @@ import java.util.regex.*;
 import java.util.Scanner;
 
 class SelfRegistration{
-	protected String name;			//to name
-	protected String age;			//to age
-	protected String Aadhar;  		//to Aadhar
-	protected String Email;			//to Email
+	protected String name;				//to name
+	protected String age;				//to age
+	protected String Aadhar;  			//to Aadhar
+	protected String Email;				//to Email
 	protected int num;				//to get random number for verification
-	protected String Mobile;		//to mobile number
+	protected String Mobile;			//to mobile number
 	protected int verify;				//to verify
 	int num1;
 	
 	void UserDetails() {					//User Login Details and validation
 		String age="^(([1-9][0-9])|(1[0][0-9]))$";				//regex age 
-		String Aadhar="^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$";		//regex Aadhar
+		String Aadhar="^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$";			//regex Aadhar
 		String Email="^[A-Za-z0-9+-._]+@(.+)$";					//regex Email
 		
 		try {
@@ -25,7 +25,7 @@ class SelfRegistration{
 			this.name = user.next();
 			
 			do {
-			System.out.print("Enter Your Age \t\t\t: ");		//getting age
+			System.out.print("Enter Your Age \t\t\t: ");			//getting age
 			this.age = user.next();
 				if(!Pattern.matches(age, this.age)) {			//regex validation age
 					System.out.println("\n-----Sorry, Please Enter Valid Age-----");
@@ -33,7 +33,7 @@ class SelfRegistration{
 			}while(!Pattern.matches(age, this.age));
 			
 			do {
-			System.out.print("Enter Your Aadhar No. \t\t: ");	//getting aadhar
+			System.out.print("Enter Your Aadhar No. \t\t: ");		//getting aadhar
 			this.Aadhar = user.next();
 				if(!Pattern.matches(Aadhar, this.Aadhar)) {		//regex validation aadhar
 					System.out.println("\n-----Sorry, Please Enter Valid Aadhar-----");
@@ -41,7 +41,7 @@ class SelfRegistration{
 			}while(!Pattern.matches(Aadhar, this.Aadhar));
 			
 			do {
-			System.out.print("Enter Your Email \t\t: ");		//getting email
+			System.out.print("Enter Your Email \t\t: ");			//getting email
 			this.Email = user.next();
 				if(!Pattern.matches(Email, this.Email)) {		//regex validation email
 					System.out.println("\n-----Sorry, Please Enter Valid Email-----");
@@ -54,14 +54,14 @@ class SelfRegistration{
 		
 	}
 
-	void MobileNo() {						//Mobile num and verification
-		String Mobile="^[6-9][0-9]{9}$";						//regex mobile
+	void MobileNo() {								//Mobile num and verification
+		String Mobile="^[6-9][0-9]{9}$";					//regex mobile
 		
 		try{
 			
 			Scanner Mob = new Scanner(System.in);
 			do {
-			System.out.print("\nEnter Your Mobile No \t\t: ");	//getting Mobile no.
+			System.out.print("\nEnter Your Mobile No \t\t: ");		//getting Mobile no.
 			this.Mobile = Mob.nextLine();
 			if(!Pattern.matches(Mobile, this.Mobile)) {			//regex validation mobile no.
 				System.out.println("\n-----Sorry, Please Enter Valid Mobile Number!-----");
@@ -73,7 +73,7 @@ class SelfRegistration{
 		}
 		
 		System.out.println();
-		Random R = new Random();								//Random Number generator
+		Random R = new Random();						//Random Number generator
 		System.out.print("Verification \t\t\t: ");
 		for (int count = 1; count <=2; count++) {
 			num = R.nextInt(9);
@@ -89,15 +89,16 @@ class SelfRegistration{
 		}while(num1 != verify);
 		System.out.println("Registered Succesfully!");
 	}
-	void Book() {												//user data output
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	void Book() {								//user data output
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		System.out.println("Registration Details!");
 		System.out.println("Name \t\t: \t"+name);
 		System.out.println("Age \t\t: \t"+age);
 		System.out.println("Aadhar No. \t: \t"+Aadhar);
 		System.out.println("Email \t\t: \t"+Email);
 		System.out.println("Mobile \t\t: \t"+Mobile);
+		System.out.println();
 		System.out.println("Note : You have to be come and collect token in center\nfrom 8 o clock to 9.30 on 21/01/2022");
-		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 	}
 }
